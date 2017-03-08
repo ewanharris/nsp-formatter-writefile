5777b1e18c5ca4dd34408a1b28e5d97b82c2f7e3
Ewan Harris <ewanharris93@gmail.com>
2017-03-08 17:28:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const Table = require('cli-table2');
+
+module.exports = function(err, data) {
+	fs.writeFileSync('scan-nsp.txt', formatLikeSummary(err, data));
+};
+
+/**
+ * Transform NSP check data into a table like the summary output of nsp check
+ * @param  {String} err  Error returned from nsp
+ * @param  {Array} data  Data returned from nsp
+ * @return {String}      Nicely formatted thing from nsp
+ */
+function formatLikeSummary(err, data) {
+	var returnString = '';
+	if (err) {
+		if (data) {
+			returnString = `(+) Debug output:	${JSON.stringify(Buffer.isBuffer(data) ? data.toString() : data)}\n`;
+			return `${returnString}${err}`
+		}
+		return `(+) ${err}`;
+	}
+
+	if (data.length === 0) {
+		return '(+) No known vulnerabilities found';
+	}
+
+	var table = new Table({
+		head: ['Name', 'Installed', 'Patched', 'Path', 'More Info'],
+		style: {
+			head: [],
+			border: []
+ 		}
+	});
+
+	data.forEach(function (finding) {
+		table.push([finding.module, finding.version, finding.patched_versions === '<0.0.0' ? 'None' : finding.patched_versions, finding.path.join(' > '), finding.advisory]);
+	});
+
+	returnString += table.toString() + '\n';
+
+	return returnString;
+}
---
+++ test.js
@@ -0,0 +1,53 @@
+const m = require('.');
+const fs = require('fs');
+const path = require('path');
+
+const readFixtureFile = (file) => {
+	return fs.readFileSync(path.join('fixtures', file), {encoding:'utf8'});
+};
+
+const readResultFile = () => {
+	return fs.readFileSync(path.join(__dirname, 'scan-nsp.txt'), {encoding:'utf8'});
+};
+
+describe('Formatting', () => {
+
+	afterEach(() => {
+		return fs.unlinkSync('scan-nsp.txt');
+	});
+
+	it('Should pretty format a warning', () => {
+		const inputData = readFixtureFile('warningsinput.txt');
+		const expected = readFixtureFile('warningsexpected.txt');
+		const input = [];
+		input.push(JSON.parse(inputData));
+		m(null, input, null);
+		const pretty = readResultFile();
+		expect(pretty).toBe(expected);
+	});
+
+	it('Should log no warnings', () => {
+		const input = [];
+		const expected = '(+) No known vulnerabilities found';
+		m(null, input, null);
+		const pretty = readResultFile();
+		expect(pretty).toBe(expected);
+	});
+
+	it('Should log an error with no data', () => {
+		const input = new Error('Something went wrong!');
+		m(input, null, null);
+		const pretty = readResultFile();
+		const expected = '(+) Error: Something went wrong!';
+		expect(pretty).toBe(expected);
+	});
+
+	it('Should log an error with data', () => {
+		const inputErr = new Error('Something went wrong!');
+		const inputData = {example: 'data'};
+		m(inputErr, inputData, null);
+		const pretty = readResultFile();
+		const expected = readFixtureFile('errorWithData.txt').trim();
+		expect(pretty).toBe(expected);
+	});
+});
